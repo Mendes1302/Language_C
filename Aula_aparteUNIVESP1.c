1c14eecab8fca0a8c9c34c674202a5d6bf44b2f0
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-08-29 16:40:18 -0300

    Update Aula_aparteUNIVESP1.c

--- Aula_aparteUNIVESP1.c
+++ Aula_aparteUNIVESP1.c
@@ -17,4 +17,30 @@ int main(void) {
   }else{
     printf(" Altura abaixo da MAXIMA\n");
   }
+  return 0;
+}
+
+//OR
+
+#include <stdio.h>
+#include <malloc.h>
+#define alturaMaxima 225
+
+//criação da primeira estrutura com alocação dinamica
+typedef struct{
+  int peso;
+  int altura;
+} PesoAltura;
+
+int main(void) {
+  PesoAltura* pessoal = (PesoAltura*) malloc(sizeof(PesoAltura));
+  pessoal->peso = 80;
+  pessoal->altura = 185;
+  printf("Peso = %i, Altura = %i.", pessoal->peso, pessoal->altura);
+  if (pessoal->altura > alturaMaxima){
+    printf(" Altura acima da MAXIMA\n");
+  }else{
+    printf(" Altura abaixo da MAXIMA\n");
+  }
+  return 0;
 }
