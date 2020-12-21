30f9b3b4740053229015e237b9064f20969960fd
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-21 19:45:08 -0300

    Create LINKED LIST.cpp

---
+++ LINKED LIST.cpp
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node
+{
+  	int nData;
+  	struct node *pLink;
+};
+
+void display( struct node *p)
+{
+  	printf("Mostrando a lista: \n");
+  	if(p)
+  	{ 
+		do
+		{
+			printf("%d", p ->nData);
+			p = p->pLink;
+		}
+		while(p);
+  	} 
+  	else
+  	{
+   		printf("Lista vazia");
+  	}	
+}
+
+int main(void)
+{
+	  struct node *pNode1 = NULL;
+	  struct node *pNode2 = NULL;
+	  struct node *pNode3 = NULL;
+
+	  pNode1 = (struct node*) malloc(sizeof(struct node*));
+	  pNode1->nData = 100;
+
+	  pNode2 = (struct node*) malloc(sizeof(struct node*));
+	  pNode2->nData = 200;
+
+	  pNode3 = (struct node*) malloc(sizeof(struct node*));
+	  pNode3->nData = 300;
+
+
+	  pNode1->pLink = pNode2;
+	  pNode2->pLink = pNode3;
+	  pNode3->pLink = NULL;
+
+	  display(pNode1);
+	  return 0;
+}
